77648acf3c36abcd48803548b6b5b0f338613181
oyyk <745691616@qq.com>
2021-09-09 20:11:32 +0800

    集成mybatis generator(未完成)

---
+++ doc/all.sql
@@ -0,0 +1,7 @@
+# 电子书表
+drop table if exists `user`;
+create table `ebook` (
+                         `id` bigint not null comment 'id',
+                         `name` varchar(50) comment '姓名',
+                         primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='用户';
--- src/main/java/com/oyyk/miaosha/controller/SampleController.java
+++ src/main/java/com/oyyk/miaosha/controller/SampleController.java
@@ -2,14 +2,22 @@ package com.oyyk.miaosha.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.xml.transform.Result;
+import java.util.AbstractList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
 
 @Controller
 @RequestMapping("/demo")
 public class SampleController {
     @RequestMapping("/thymeleaf")
-    public String thymeleaf(Model model){
-        model.addAttribute("name","oyyk");
+    public String thymeleaf(Model model) {
+        model.addAttribute("name", "oyyk");
         return "hello";
     }
+
 }
