f6a1168e923a1560b5eb8a9f2fe737b399e83dc6
oyyk <745691616@qq.com>
2021-09-10 17:23:53 +0800

    2-7 解决generator集成未完成的部分。

--- doc/all.sql
+++ doc/all.sql
@@ -1,7 +1,16 @@
-# 电子书表
+# 用户表
 drop table if exists `user`;
 create table `user` (
                          `id` bigint not null comment 'id',
                          `name` varchar(50) comment '姓名',
                          primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='用户';
+
+
+# 测试表
+drop table if exists `test`;
+create table `test` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '姓名',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
